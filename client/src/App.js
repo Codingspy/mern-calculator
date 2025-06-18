32b157b10e52966a42addc9e92cfc49c8334e62b
Codingspy <praveenshinde2022mite@gmail.com>
2025-06-18 23:12:31 +0530

    Configured for deployment

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ function App() {
 
   const calculate = async (expression) => {
     try {
-      const res = await fetch('http://localhost:5000/calculate', {
+      const res = await fetch('https://mern-calculator-api.onrender.com/calculate', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ expression }),
